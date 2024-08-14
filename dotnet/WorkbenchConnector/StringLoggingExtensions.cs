4b4090b64ba1f06e1eff02cb902aa27aef5237d8
Devis Lucato <dluc@users.noreply.github.com>
2024-08-13 18:09:13 -0700

    [.NET] Safe logging (#18)
    Improve logging security by encoding user input parameters when logged

---
+++ dotnet/WorkbenchConnector/StringLoggingExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.SemanticWorkbench.Connector;
+
+public static class StringLoggingExtensions
+{
+    public static string HtmlEncode(this string? value)
+    {
+        return string.IsNullOrWhiteSpace(value)
+            ? $"{value}"
+            : HttpUtility.HtmlEncode(value);
+    }
+
+    public static string HtmlEncode(this PathString value)
+    {
+        return string.IsNullOrWhiteSpace(value)
+            ? $"{value}"
+            : HttpUtility.HtmlEncode(value);
+    }
+
+    public static string HtmlEncode(this StringBuilder value)
+    {
+        var s = value.ToString();
+        return string.IsNullOrWhiteSpace(s)
+            ? $"{s}"
+            : HttpUtility.HtmlEncode(s);
+    }
+
+    public static string HtmlEncode(this object? value)
+    {
+        return value == null
+            ? string.Empty
+            : HttpUtility.HtmlEncode(value.ToString() ?? string.Empty);
+    }
+}
