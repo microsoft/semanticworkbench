e90066485caab92137d80ee3519f17aced056e4a
Mark Waddle <mark@markwaddle.com>
2024-09-30 23:31:58 +0000

    Make workbench service url configurable

--- workbench-app/src/libs/AuthHelper.ts
+++ workbench-app/src/libs/AuthHelper.ts
@@ -6,47 +6,35 @@ import { Constants } from '../Constants';
 
 const log = debug(Constants.debug.root).extend('auth-helper');
 
-const getMsalConfig = () => {
-    const envClientId: string | undefined = import.meta.env.VITE_SEMANTIC_WORKBENCH_CLIENT_ID;
-    const envAuthority: string | undefined = import.meta.env.VITE_SEMANTIC_WORKBENCH_AUTHORITY;
-    const clientId = envClientId || Constants.msal.auth.clientId;
-    const authority = envAuthority || Constants.msal.auth.authority;
-
-    log(`Using clientId: ${clientId} (from env: ${!!envClientId})`);
-    log(`Using authority: ${authority} (from env: ${!!envAuthority})`);
-
-    return {
-        auth: {
-            ...Constants.msal.auth,
-            clientId,
-            authority,
-            redirectUri: window.origin,
-        },
-        cache: Constants.msal.cache,
-        system: {
-            loggerOptions: {
-                loggerCallback: (level: any, message: any, containsPii: any) => {
-                    if (containsPii) {
+const getMsalConfig = () => ({
+    auth: {
+        ...Constants.msal.auth,
+        redirectUri: window.origin,
+    },
+    cache: Constants.msal.cache,
+    system: {
+        loggerOptions: {
+            loggerCallback: (level: any, message: any, containsPii: any) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        log('error:', message);
                         return;
-                    }
-                    switch (level) {
-                        case LogLevel.Error:
-                            log('error:', message);
-                            return;
-                        // case LogLevel.Info:
-                        //     log('info:', message);
-                        //     return;
-                        // case LogLevel.Verbose:
-                        //     log('verbose:', message);
-                        //     return;
-                        case LogLevel.Warning:
-                            log('warn:', message);
-                    }
-                },
+                    // case LogLevel.Info:
+                    //     log('info:', message);
+                    //     return;
+                    // case LogLevel.Verbose:
+                    //     log('verbose:', message);
+                    //     return;
+                    case LogLevel.Warning:
+                        log('warn:', message);
+                }
             },
         },
-    };
-};
+    },
+});
 
 const loginRequest: RedirectRequest = {
     scopes: Constants.msal.msGraphScopes,
