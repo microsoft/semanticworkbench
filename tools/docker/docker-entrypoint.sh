0fa0f251abd8e1c619d0dddcdabfb8980b3697f1
Mark Waddle <markwaddle@users.noreply.github.com>
2024-09-30 14:14:18 -0700

    Deploy workbench-service from public repo (#63)

---
+++ tools/docker/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# if SSHD_PORT is set, start sshd
+if [ -n "${SSHD_PORT}" ]; then
+    service ssh start
+fi
+
+exec "$@"
--- workbench-service/scripts/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-service ssh start
-
-start-semantic-workbench-service --port ${port}
