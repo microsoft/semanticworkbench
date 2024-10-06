28ddb980d5902242daee547675d49d160c1885a3
Mark Waddle <mark@markwaddle.com>
2024-10-06 02:44:43 +0000

    Fixes port env vars in dockerfiles

--- libraries/python/semantic-workbench-assistant/semantic_workbench_assistant/settings.py
+++ libraries/python/semantic-workbench-assistant/semantic_workbench_assistant/settings.py
@@ -1,5 +1,6 @@
-from typing import Annotated, Literal
-from pydantic import AliasChoices, Field, HttpUrl
+from typing import Literal
+
+from pydantic import Field, HttpUrl
 from pydantic_core import Url
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -16,30 +17,8 @@ class Settings(BaseSettings):
     storage: FileStorageSettings = FileStorageSettings(root=".data/assistants")
     logging: LoggingSettings = LoggingSettings()
 
-    workbench_service_url: Annotated[
-        HttpUrl,
-        Field(
-            # alias for backwards compatibility with older env vars
-            validation_alias=AliasChoices(
-                "assistant__workbench_service_url",
-                "ASSISTANT__WORKBENCH_SERVICE_URL",
-                "assistant__workbench_service_base_url",
-                "ASSISTANT__WORKBENCH_SERVICE_BASE_URL",
-            )
-        ),
-    ] = Url("http://127.0.0.1:3000")
-    workbench_service_api_key: Annotated[
-        str,
-        Field(
-            # alias for backwards compatibility with older env vars
-            validation_alias=AliasChoices(
-                "assistant__api_key",
-                "ASSISTANT__API_KEY",
-                "assistant__workbench_service_api_key",
-                "ASSISTANT__WORKBENCH_SERVICE_API_KEY",
-            )
-        ),
-    ] = ""
+    workbench_service_url: HttpUrl = Url("http://127.0.0.1:3000")
+    workbench_service_api_key: str = ""
     workbench_service_ping_interval_seconds: float = 20.0
 
     assistant_service_id: str | None = None
@@ -50,7 +29,7 @@ class Settings(BaseSettings):
 
     protocol: Literal["http", "https"] = "http"
     host: str = "127.0.0.1"
-    port: int = 3001
+    port: int = 0
 
     website_protocol: str = Field(alias="WEBSITE_PROTOCOL", default="https")
     website_port: int | None = Field(alias="WEBSITE_PORT", default=None)
