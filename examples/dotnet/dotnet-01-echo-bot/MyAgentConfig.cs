75510a6e4b47c1e929a1e2a709d4774648bd752e
Devis Lucato <devis@microsoft.com>
2024-10-09 13:07:33 -0700

    Upgrade .NET examples
    * Use latest .NET connector nuget
    * Upgrade SK libs
    * Improve run-app.sh to auto select node.js 20.x

--- examples/dotnet/dotnet-01-echo-bot/MyAgentConfig.cs
+++ examples/dotnet/dotnet-01-echo-bot/MyAgentConfig.cs
@@ -5,66 +5,21 @@ using Microsoft.SemanticWorkbench.Connector;
 
 namespace AgentExample;
 
-public class MyAgentConfig : IAgentConfig
+public class MyAgentConfig : AgentConfig
 {
     [JsonPropertyName(nameof(this.ReplyToAgents))]
     [JsonPropertyOrder(10)]
+    [AgentConfigProperty("type", "boolean")]
+    [AgentConfigProperty("title", "Reply to other assistants in conversations")]
+    [AgentConfigProperty("description", "Reply to assistants")]
+    [AgentConfigProperty("default", false)]
     public bool ReplyToAgents { get; set; } = false;
 
     [JsonPropertyName(nameof(this.CommandsEnabled))]
     [JsonPropertyOrder(20)]
+    [AgentConfigProperty("type", "boolean")]
+    [AgentConfigProperty("title", "Support commands")]
+    [AgentConfigProperty("description", "Support commands, e.g. /say")]
+    [AgentConfigProperty("default", false)]
     public bool CommandsEnabled { get; set; } = false;
-
-    public void Update(object? config)
-    {
-        if (config == null)
-        {
-            throw new ArgumentException("Incompatible or empty configuration");
-        }
-
-        if (config is not MyAgentConfig cfg)
-        {
-            throw new ArgumentException("Incompatible configuration type");
-        }
-
-        this.ReplyToAgents = cfg.ReplyToAgents;
-        this.CommandsEnabled = cfg.CommandsEnabled;
-    }
-
-    public object ToWorkbenchFormat()
-    {
-        Dictionary<string, object> result = new();
-        Dictionary<string, object> defs = new();
-        Dictionary<string, object> properties = new();
-        Dictionary<string, object> jsonSchema = new();
-        Dictionary<string, object> uiSchema = new();
-
-        properties[nameof(this.ReplyToAgents)] = new Dictionary<string, object>
-        {
-            { "type", "boolean" },
-            { "title", "Reply to other assistants in conversations" },
-            { "description", "Reply to assistants" },
-            { "default", false }
-        };
-
-        properties[nameof(this.CommandsEnabled)] = new Dictionary<string, object>
-        {
-            { "type", "boolean" },
-            { "title", "Support commands" },
-            { "description", "Support commands, e.g. /say" },
-            { "default", false }
-        };
-
-        jsonSchema["type"] = "object";
-        jsonSchema["title"] = "ConfigStateModel";
-        jsonSchema["additionalProperties"] = false;
-        jsonSchema["properties"] = properties;
-        jsonSchema["$defs"] = defs;
-
-        result["json_schema"] = jsonSchema;
-        result["ui_schema"] = uiSchema;
-        result["config"] = this;
-
-        return result;
-    }
 }
