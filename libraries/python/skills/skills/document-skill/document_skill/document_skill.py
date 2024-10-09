d861712f2ab71ffe91c0ac849d2e557528fe0ce8
Mollie Munoz <mollie.munoz@microsoft.com>
2024-10-09 00:16:38 +0000

    More notes and skill name fix

--- libraries/python/skills/skills/document-skill/document_skill/document_skill.py
+++ libraries/python/skills/skills/document-skill/document_skill/document_skill.py
@@ -1,11 +1,10 @@
-
+from chat_driver import ChatDriverConfig
 from context import Context
 from pydantic import BaseModel  # temp to have something to experiment with
 from skill_library import RoutineTypes, Skill
 from skill_library.routine import InstructionRoutine, ProgramRoutine
-from chat_driver import ChatDriverConfig
 
-NAME = "Document skill"
+NAME = "document_skill"
 DESCRIPTION = "Anything related to documents - creation, edit, translation"
 
 
@@ -30,9 +29,7 @@ class Paper(BaseModel):
 
 
 class DocumentSkillContext(Context):
-    def __init__(
-        self
-    ) -> None:
+    def __init__(self) -> None:
         self.chat_history: str = ""
         self.attachments_list: list[Attachment] = []
         self.outline_versions: list[Outline] = []
@@ -46,13 +43,25 @@ class DocumentSkill(Skill):
         self,
         context: Context,
     ) -> None:
-
         self.document_skill_context: DocumentSkillContext = DocumentSkillContext()
 
         actions = [
             self.test_action,
         ]
 
+        # however, with current skill chat driver config (no chat connection setup),
+        # i see an issue when running the instruction routine as a command.  It correctly detects the
+        # test_action step to run... but the I get the response: "Error running function: 'NoneType' object has no attribute 'chat'"
+        # it looks like information events after actions run in posix command are passed through a chat driver with model
+        # connectivity... as it explains the result in a natural language response wrapper (not the simple result of directly calling each command).
+        # i'm wondering if this is where the skill chat driver gets used as a chat connection?
+        # YES.  look at instruction_routine_runner.  This is how it is designed currently.
+        # i.r.r. will use the configured chat driver of the routine's skill to call "respond" with message being the "step" (the function in the routine)...
+        # but this means it is passed to the language model with the tool choices (assuming the action/function has also been registered as a function not just
+        # as a command... so that it can be included in the tool selection for the skill's chat driver to pass to the language model). Our code will execute (code in the
+        # chat driver... i think... i need to look into chat driver code more...) but the wrapped response out of the respond function is returned and messaged
+        # out as an information event.
+        # so big Q here... should a program routine or instruction routine be run by a language model?  I'm not sure I want this... can we have an option?
         routines: list[RoutineTypes] = [
             self.test_instruction_routine(),
         ]
@@ -85,7 +94,7 @@ class DocumentSkill(Skill):
         Update this routine description.
         """
         return InstructionRoutine(
-            name="template_example",
+            name="test_instruction_routine",
             description="Description of what the routine does.",
             routine=("test_action"),
         )
@@ -95,7 +104,7 @@ class DocumentSkill(Skill):
         Update this routine description.
         """
         return ProgramRoutine(
-            name="template_example",
+            name="template_example_routine",
             description="Description of what the routine does.",
             program=("TBD"),
         )
@@ -107,9 +116,11 @@ class DocumentSkill(Skill):
         self,
         context: Context,
     ) -> str:
-        return "hello world"
+        return "hello world"  #
 
     # This may not be needed as an action... not sure yet how context will be set.
+
+
 #    def set_context(
 #        self,
 #        context: Context,
@@ -311,7 +322,7 @@ class DocumentSkill(Skill):
 #
 #
 ## Current approach uses system messages for everything.
-#draft_outline_main_system_message = ("Generate an outline for the document, including title. The outline should include the key points that will"
+# draft_outline_main_system_message = ("Generate an outline for the document, including title. The outline should include the key points that will"
 #    " be covered in the document. Consider the attachments, the rationale for why they were uploaded, and the"
 #    " conversation that has taken place. The outline should be a hierarchical structure with multiple levels of"
 #    " detail, and it should be clear and easy to understand. The outline should be generated in a way that is"
@@ -321,12 +332,12 @@ class DocumentSkill(Skill):
 #    # " outline if it exists, as well as any other attachments provided by the user to inform a newly revised "
 #    # "outline draft. Provide ONLY any outline. Provide no further instructions to the user.")
 #
-#outline_feedback_main_system_message = ("Use the user's most recent feedback to determine if the user wants to iterate further on the"
+# outline_feedback_main_system_message = ("Use the user's most recent feedback to determine if the user wants to iterate further on the"
 #    " provided outline [ITERATE], or if the user is ready to move on to drafting a paper from the"
 #    " provided outline [CONTINUE]. Based on the user's feedback on the provided outline, determine if"
 #    " the user wants to [ITERATE], [CONTINUE], or [QUIT]. Reply with ONLY [ITERATE], [CONTINUE], or [QUIT].")
 #
-#draft_page_continue_main_system_message = ("Following the structure of the outline, create the content for the next (or first) page of the"
+# draft_page_continue_main_system_message = ("Following the structure of the outline, create the content for the next (or first) page of the"
 #    " document - don't try to create the entire document in one pass nor wrap it up too quickly, it will be a"
 #    " multi-page document so just create the next page. It's more important to maintain"
 #    " an appropriately useful level of detail. After this page is generated, the system will follow up"
@@ -340,32 +351,32 @@ class DocumentSkill(Skill):
 #    # " This way the user can review newly drafted content as a subset of the future full document and not be overwhelmed."
 #    # "Only provide the newly drafted content. Provide no further instructions to the user.")
 #
-#draft_page_iterate_main_system_message = ("Following the structure of the outline, iterate on the currently drafted page of the"
+# draft_page_iterate_main_system_message = ("Following the structure of the outline, iterate on the currently drafted page of the"
 #    " document. It's more important to maintain"
 #    " an appropriately useful level of detail. After this page is iterated upon, the system will follow up"
 #    " and ask for the next page.")
 #
-#draft_page_feedback_main_system_message = ("You are an AI assistant that helps draft outlines for a future flushed-out document."
+# draft_page_feedback_main_system_message = ("You are an AI assistant that helps draft outlines for a future flushed-out document."
 #    " You use the user's most recent feedback to determine if the user wants to iterate further on the"
 #    " provided draft content [ITERATE], or if the user is ready to move on to drafting new additional content"
 #    " [CONTINUE]. Based on the user's feedback on the provided drafted content, determine if"
 #    " the user wants to [ITERATE], [CONTINUE], or [QUIT]. Reply with ONLY [ITERATE], [CONTINUE], or [QUIT].")
 #
-#def _add_main_system_message(messages: list[ChatCompletionMessageParam], prompt: str) -> None:
+# def _add_main_system_message(messages: list[ChatCompletionMessageParam], prompt: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": prompt
 #    }
 #    messages.append(message)
 #
-#def _add_chat_history_system_message(messages: list[ChatCompletionMessageParam], chat_history: str) -> None:
+# def _add_chat_history_system_message(messages: list[ChatCompletionMessageParam], chat_history: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": f"<CHAT_HISTORY>{chat_history}</CHAT_HISTORY>"
 #    }
 #    messages.append(message)
 #
-#def _add_attachments_system_message(messages: list[ChatCompletionMessageParam], attachments: list[Attachment]) -> None:
+# def _add_attachments_system_message(messages: list[ChatCompletionMessageParam], attachments: list[Attachment]) -> None:
 #    for item in attachments:
 #        message: ChatCompletionSystemMessageParam = {
 #            "role": "system",
@@ -373,28 +384,28 @@ class DocumentSkill(Skill):
 #        }
 #        messages.append(message)
 #
-#def _add_existing_outline_system_message(messages: list[ChatCompletionMessageParam], outline: str) -> None:
+# def _add_existing_outline_system_message(messages: list[ChatCompletionMessageParam], outline: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": (f"<EXISTING_OUTLINE>{outline}</EXISTING_OUTLINE>")
 #    }
 #    messages.append(message)
 #
-#def _add_approved_outline_system_message(messages: list[ChatCompletionMessageParam], outline: str) -> None:
+# def _add_approved_outline_system_message(messages: list[ChatCompletionMessageParam], outline: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": (f"<APPROVED_OUTLINE>{outline}</APPROVED_OUTLINE>")
 #    }
 #    messages.append(message)
 #
-#def _add_existing_content_system_message(messages: list[ChatCompletionMessageParam], content: str) -> None:
+# def _add_existing_content_system_message(messages: list[ChatCompletionMessageParam], content: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": (f"<EXISTING_CONTENT>{content}</EXISTING_CONTENT>")
 #    }
 #    messages.append(message)
 #
-#def _add_user_feedback_system_message(messages: list[ChatCompletionMessageParam], user_feedback: str) -> None:
+# def _add_user_feedback_system_message(messages: list[ChatCompletionMessageParam], user_feedback: str) -> None:
 #    message: ChatCompletionSystemMessageParam = {
 #        "role": "system",
 #        "content": (f"<USER_FEEDBACK>{user_feedback}</USER_FEEDBACK>")
