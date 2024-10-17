a87e1c070f16c7d73a06334a510329ff8f226593
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-17 19:17:50 +0200

    aspire-ize semantic workbench
    using localhost globally
    
    working example with aspire and uvicorn integration
    
    dynamic python port
    
    python assistant port defined via env var
    
    python assistant read workbench service url from env
    
    reverting port changes for python services
    
    using correct env var for python assistant and dockerfile for FE
    
    use custom env var for workbench service endpoint in app
    
    correct injection of workbench servic endoint
    
    ensure correct env var are used in dotnet example 3
    
    deploy via azd using workaround for dockerfile
    
    using correct env var for workbench_service url in python assistants
    
    working deployment with azd
    
    don't pass port during frontend docker build - not needed
    
    working deployment with hardcoded appids in wk service
    
    update doc
    
    app registration id is now picked from .env in docker
    
    working deployment of dotnet example
    
    working assistant deployment
    
    reorders apphost
    
    reorders packages
    
    cleans up
    
    adds doc
    
    uses new start assistant
    
    imrpove doc
    
    updates doc
    
    fixes assistant container params
    
    differentiate aspire dockerfile from already existing ones
    
    cleans workbench app dockerfile
    
    Minor code style changes
    
    back to 127.0.0.1 on workbench config
    
    fixes workbench app docker-entrypoint logs
    
    revert space on start
    
    removed next-steps.md
    
    removes sshd from Dockerfile workbench service and assistant
    
    reverts .env.example
    
    reverts settings.py
    
    improve readme
    
    improves readme requirements
    
    fixes folder for apphost in doc
    
    Update aspire-orchestrator/SemanticWorkbench.Aspire.Hosting.Extensions/PathNormalizer.cs
    
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>
    
    Update aspire-orchestrator/SemanticWorkbench.Aspire.Hosting.Extensions/PathNormalizer.cs
    
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>
    
    using arg value for vite_semanticworkbenchurl
    
    moves dockerfileextensions to separate class
    
    aspire overwrites correct var env for agents
    
    adds copyright
    
    Revert "using arg value for vite_semanticworkbenchurl"
    
    This reverts commit b0d15aef565dde0c8ab3fe36734b58f5b24c8e8a.
    
    deletes aspire-manifest
    
    uses workaround for workbench service url in frontend
    
    dockerignore .env files
    
    dockerignore .azure folder
    
    adds comment on agent autoreference
    
    adds doc for dotnet dev certs
    
    use env var for entra ID config
    
    read entraid config from appsettings
    
    adds default values for app registration
    
    Code style
    
    Update workbench-service/.env.example
    
    Add comment to new docker file
    
    Code style
    
    Revert code style changes
    
    Revert changes to agent 3 csproj
    
    Update examples/dotnet/dotnet-03-simple-chatbot/Program.cs
    
    Update examples/dotnet/dotnet-03-simple-chatbot/dotnet-03-simple-chatbot.csproj
    
    Update libraries/python/semantic-workbench-assistant/semantic_workbench_assistant/settings.py
    
    Update workbench-app/src/Constants.ts
    
    Update workbench-app/src/global.d.ts
    
    Fix SLN filename
    
    Fix project names
    
    Revert code style changes
    
    Revert changes to agent 3 csproj
    
    Update examples/dotnet/dotnet-03-simple-chatbot/Program.cs
    
    Update examples/dotnet/dotnet-03-simple-chatbot/dotnet-03-simple-chatbot.csproj
    
    Update libraries/python/semantic-workbench-assistant/semantic_workbench_assistant/settings.py
    
    Update workbench-app/src/Constants.ts
    
    Update workbench-app/src/global.d.ts
    
    Fix SLN filename
    
    Fix project names
    
    Update libraries/python/semantic-workbench-assistant/semantic_workbench_assistant/settings.py
    
    Update workbench-app/src/Constants.ts
    
    Update workbench-app/src/global.d.ts
    
    Fix SLN filename
    
    Fix project names
    
    Reorg projects
    
    Fix URLs and ping errors
    
    Handle ping exceptions
    
    Improve logging
    
    Change ping frequency
    
    Auto detect port in use and better handle some exceptions
    
    Rebasing
    
    Update workbench-service/.env.example
    
    Update workbench-app/.env.example
    
    Fix merge
    
    Fix merge
    
    Fix merge

---
+++ aspire-orchestrator/Aspire.Extensions/DockerFileExtensions.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Aspire.Hosting;
+
+public static class DockerFileExtensions
+{
+    public static IResourceBuilder<ExecutableResource> PublishAsDockerImage(this IResourceBuilder<ExecutableResource> builder,
+        string? dockerContext = null,
+        string? dockerFilePath = "Dockerfile",
+        Action<IResourceBuilder<ContainerResource>>? configure = null)
+    {
+        if (!builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            return builder;
+        }
+
+        // Bait and switch the ExecutableResource with a ContainerResource
+        builder.ApplicationBuilder.Resources.Remove(builder.Resource);
+
+        var container = new ExecutableContainerResource(builder.Resource);
+        var cb = builder.ApplicationBuilder.AddResource(container);
+        cb.WithImage(builder.Resource.Name);
+        cb.WithDockerfile(contextPath: dockerContext ?? builder.Resource.WorkingDirectory, dockerfilePath: dockerFilePath);
+        // Clear the runtime args
+        cb.WithArgs(c => c.Args.Clear());
+
+        configure?.Invoke(cb);
+
+        return builder;
+    }
+
+    private sealed class ExecutableContainerResource(ExecutableResource er) : ContainerResource(er.Name)
+    {
+        public override ResourceAnnotationCollection Annotations => er.Annotations;
+    }
+}
