c14c7afa013366498c5b99f126547f7a66a00f36
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-30 20:49:44 +0100

    python assistant read workbench service url from env

--- aspire-orchestrator/SemanticWorkench.Aspire.AppHost/Program.cs
+++ aspire-orchestrator/SemanticWorkench.Aspire.AppHost/Program.cs
@@ -2,19 +2,26 @@ using CommunityToolkit.Aspire.Hosting.Uvicorn;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var workbenchService = builder.AddUvicornApp("worbenchservice", projectDirectory: Path.Combine("..", "..", "workbench-service"), scriptPath: "start-semantic-workbench-service")
+
+var workbenchService = builder.AddUvicornApp("worbenchservice",
+                                            projectDirectory: Path.Combine("..", "..", "workbench-service"),
+                                            scriptPath: "start-semantic-workbench-service")
     .WithHttpEndpoint(env: "PORT");
 
-// var workbenchService = builder.AddExecutable("worbenchservice", @".\.venv\Scripts\start-semantic-workbench-service.exe", Path.Combine("..", "..", "workbench-service"))
-//     .WithHttpEndpoint(targetPort: 3000, isProxied: true)
-//     .WithHttpHealthCheck("/");
 var workbenchServiceEndpoint = workbenchService.GetEndpoint("http");
 
-var agent3 = builder.AddProject<Projects.dotnet_03_simple_chatbot>("agent3")
+var dotnetAgent3 = builder.AddProject<Projects.dotnet_03_simple_chatbot>("agent3")
     .WithReference(workbenchServiceEndpoint)
     .WaitFor(workbenchService);
+dotnetAgent3.WithReference(dotnetAgent3);
 
-agent3.WithReference(agent3);
+var pythonAgent1 = builder.AddUvicornApp("pythonAgent",
+                                        projectDirectory: Path.Combine("..", "..", "examples", "python", "python-01-echo-bot"),
+                                        scriptPath: "start-semantic-workbench-assistant",
+                                        scriptArgs: ["assistant.chat:app"])
+    .WithHttpEndpoint(env: "PORT")
+    .WithReference(workbenchServiceEndpoint);
+
 
 builder.AddViteApp("workbenchapp", workingDirectory: Path.Combine("..", "..", "workbench-app"), packageManager: "pnpm")
     .WithPnpmPackageInstallation()
