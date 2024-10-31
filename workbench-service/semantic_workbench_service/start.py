395ac99cbed89dbbc104a0c97a0a23d45b7754d9
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-31 10:14:32 +0100

    reverting port changes for python services

--- workbench-service/semantic_workbench_service/start.py
+++ workbench-service/semantic_workbench_service/start.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import os
 
 import uvicorn
 from fastapi import FastAPI
@@ -34,7 +33,7 @@ def main():
         help="host IP to run service on",
     )
     parse_args.add_argument(
-        "--port", dest="port", type=int, default=int(os.getenv("PORT") or settings.service.port), help="port to run service on"
+         "--port", dest="port", type=int, default=settings.service.port, help="port to run service on"
     )
     args = parse_args.parse_args()
 
