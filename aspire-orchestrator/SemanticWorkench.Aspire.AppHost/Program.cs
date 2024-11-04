521a63b0f3ad6508e18caa3b6ea140f4da953df3
Tommaso Stocchi <tstocchi@microsoft.com>
2024-11-04 21:08:01 +0100

    correct injection of workbench servic endoint

--- aspire-orchestrator/SemanticWorkench.Aspire.AppHost/Program.cs
+++ aspire-orchestrator/SemanticWorkench.Aspire.AppHost/Program.cs
@@ -3,7 +3,7 @@ using CommunityToolkit.Aspire.Hosting.Uvicorn;
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-var workbenchService = builder.AddUvicornApp("worbenchservice",
+var workbenchService = builder.AddUvicornApp("workbenchservice",
                                             projectDirectory: Path.Combine("..", "..", "workbench-service"),
                                             scriptPath: "start-semantic-workbench-service")
     .WithHttpEndpoint(env: "PORT");
@@ -15,18 +15,19 @@ var dotnetAgent3 = builder.AddProject<Projects.dotnet_03_simple_chatbot>("agent3
     .WaitFor(workbenchService);
 dotnetAgent3.WithReference(dotnetAgent3);
 
-var pythonAgent1 = builder.AddUvicornApp("pythonAgent",
-                                        projectDirectory: Path.Combine("..", "..", "examples", "python", "python-01-echo-bot"),
-                                        scriptPath: "start-semantic-workbench-assistant",
-                                        scriptArgs: ["assistant.chat:app"])
-    .WithHttpEndpoint(env: "ASSISTANT__PORT")
-    .WithReference(workbenchServiceEndpoint);
+// var pythonAgent1 = builder.AddUvicornApp("pythonAgent",
+//                                         projectDirectory: Path.Combine("..", "..", "examples", "python", "python-01-echo-bot"),
+//                                         scriptPath: "start-semantic-workbench-assistant",
+//                                         scriptArgs: ["assistant.chat:app"])
+//     .WithHttpEndpoint(env: "ASSISTANT__PORT")
+//     .WithReference(workbenchServiceEndpoint);
 
 
 builder.AddViteApp("workbenchapp", workingDirectory: Path.Combine("..", "..", "workbench-app"), packageManager: "pnpm")
     .WithPnpmPackageInstallation()
     .WithHttpsEndpoint(env: "PORT")
-    .WithReference(workbenchServiceEndpoint)
+    // .WithReference(workbenchServiceEndpoint)
+    .WithEnvironment(name: "VITE_SEMANTIC_WORKBENCH_SERVICE_URL", workbenchServiceEndpoint)
     .WaitFor(workbenchService)
     .PublishAsDockerFile();
 
