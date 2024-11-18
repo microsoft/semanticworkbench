18997a51d5a004f935effead624683b18a2478cb
Paul Payne <papayne@microsoft.com>
2024-11-18 19:05:53 +0000

    Exposes some chat driver objects for easier import.

--- libraries/python/openai-client/openai_client/__init__.py
+++ libraries/python/openai-client/openai_client/__init__.py
@@ -3,6 +3,7 @@ import logging
 from .client import (
     create_client,
 )
+from .completion import message_content_from_completion, message_from_completion
 from .config import (
     AzureOpenAIApiKeyAuthConfig,
     AzureOpenAIAzureIdentityAuthConfig,
@@ -10,7 +11,20 @@ from .config import (
     OpenAIServiceConfig,
     ServiceConfig,
 )
+from .errors import (
+    CompletionError,
+    validate_completion,
+)
+from .logging import (
+    add_serializable_data,
+    make_completion_args_serializable,
+)
 from .messages import (
+    create_assistant_message,
+    create_system_message,
+    create_user_message,
+    format_with_dict,
+    format_with_liquid,
     truncate_messages_for_logging,
 )
 from .tokens import (
@@ -23,15 +37,26 @@ logger = logging.getLogger(__name__)
 
 
 __all__ = [
-    "create_client",
-    "truncate_messages_for_logging",
-    "num_tokens_from_message",
-    "num_tokens_from_messages",
-    "num_tokens_from_tools_and_messages",
+    "add_serializable_data",
     "AzureOpenAIApiKeyAuthConfig",
     "AzureOpenAIAzureIdentityAuthConfig",
     "AzureOpenAIServiceConfig",
+    "CompletionError",
+    "create_client",
+    "create_assistant_message",
+    "create_system_message",
+    "create_user_message",
+    "format_with_dict",
+    "format_with_liquid",
+    "logger",
+    "make_completion_args_serializable",
+    "message_content_from_completion",
+    "message_from_completion",
+    "num_tokens_from_message",
+    "num_tokens_from_messages",
+    "num_tokens_from_tools_and_messages",
     "OpenAIServiceConfig",
     "ServiceConfig",
-    "logger",
+    "truncate_messages_for_logging",
+    "validate_completion",
 ]
