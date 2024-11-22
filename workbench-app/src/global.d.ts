1ab73235bc78358363fe61dcb861e127d77ff524
Devis Lucato <dluc@users.noreply.github.com>
2024-11-22 13:22:10 -0800

    Update workbench-app/src/global.d.ts

--- workbench-app/src/global.d.ts
+++ workbench-app/src/global.d.ts
@@ -1,6 +1,6 @@
 export { };
 
-// Allow static build of REact code to access env vars
+// Allow static build of React code to access env vars
 // SEE https://create-react-app.dev/docs/title-and-meta-tags/#injecting-data-from-the-server-into-the-page
 declare global {
   interface Window {
