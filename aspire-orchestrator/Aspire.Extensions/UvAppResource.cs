590e67ec69ce201196f2aac36228595bd65a0d15
Devis Lucato <devis@microsoft.com>
2024-11-25 15:56:53 -0800

    Remove Aspire files

--- aspire-orchestrator/Aspire.Extensions/UvAppResource.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-
-namespace Aspire.Hosting.ComponentModel;
-
-public class UvAppResource(string name, string workingDirectory)
-    : ExecutableResource(name, "uv", workingDirectory), IResourceWithServiceDiscovery
-{
-    internal const string HttpEndpointName = "http";
-}
