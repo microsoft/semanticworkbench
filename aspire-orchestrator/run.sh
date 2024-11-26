9ac74aaf2c3d3ae9221b9a823cc2ecf21d55242b
Tommaso Stocchi <tstocchi@microsoft.com>
2024-11-26 22:48:14 +0100

    fixes run.sh merge

--- aspire-orchestrator/run.sh
+++ aspire-orchestrator/run.sh
@@ -5,37 +5,6 @@ set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
 cd "$HERE"
 
-<<<<<<< HEAD
-=======
-# Check node version, it must be major version 20 (any minor), otherwise show an error and exit
-NODE_VERSION=$(node -v)
-if [[ ! $NODE_VERSION =~ ^v(1[8-9]|[2-9][0-9]).* ]]; then
-    echo "Node version is $NODE_VERSION, expected 18.x.x or higher."
-
-  # Attempt to source nvm
-  if [ -s "$NVM_DIR/nvm.sh" ]; then
-    . "$NVM_DIR/nvm.sh"
-  elif [ -s "$HOME/.nvm/nvm.sh" ]; then
-    export NVM_DIR="$HOME/.nvm"
-    . "$NVM_DIR/nvm.sh"
-  else
-    echo "nvm not found. Please install Node 18 or higher manually."
-    echo "See also README.md for instructions."
-    exit 1
-  fi
-
-  echo "Installing latest LTS Node version via nvm..."
-  nvm install --lts
-  nvm use --lts
-
-  NODE_VERSION=$(node -v)
-  if [[ ! $NODE_VERSION =~ ^v(1[8-9]|[2-9][0-9]).* ]]; then
-    echo "Failed to switch to Node 18 or higher via nvm. You have $NODE_VERSION."
-    exit 1
-  fi
-fi
-
->>>>>>> 1f6d7e847c4d4d17910ddd7c53f2b9faaaa29bcd
 cd Aspire.AppHost
 
 dotnet run
