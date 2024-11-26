74707f1f25ff9cfff83d13d60951a576ba0b5191
Devis Lucato <dluc@users.noreply.github.com>
2024-11-25 16:08:03 -0800

    Update workbench-app/.env.example

--- workbench-app/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-export { };
-
-// Allow static build of React code to access env vars
-// SEE https://create-react-app.dev/docs/title-and-meta-tags/#injecting-data-from-the-server-into-the-page
-declare global {
-  interface Window {
-    VITE_SEMANTIC_WORKBENCH_SERVICE_URL?: string;
-  }
-}
