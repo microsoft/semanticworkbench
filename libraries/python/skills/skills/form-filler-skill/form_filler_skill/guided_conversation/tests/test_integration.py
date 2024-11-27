370e1eee63f06acea7c5e7f36f15f169acd032fb
Paul Payne <papayne@microsoft.com>
2024-11-27 00:27:47 +0000

    Fixes test.

--- libraries/python/skills/skills/form-filler-skill/form_filler_skill/guided_conversation/tests/test_integration.py
+++ libraries/python/skills/skills/form-filler-skill/form_filler_skill/guided_conversation/tests/test_integration.py
@@ -1,18 +1,13 @@
 from pathlib import Path
-from textwrap import dedent
+from typing import Any
 
 import pytest
 from assistant_drive import Drive, DriveConfig
-from form_filler_skill.guided_conversation.definition import GCDefinition
+from form_filler_skill.guided_conversation.definitions.acrostic_poem import definition
 from form_filler_skill.guided_conversation.guided_conversation_skill import GuidedConversationSkill
-from form_filler_skill.guided_conversation.resources import (
-    ResourceConstraint,
-    ResourceConstraintMode,
-    ResourceConstraintUnit,
-)
 from openai_client import AzureOpenAIAzureIdentityAuthConfig, AzureOpenAIServiceConfig, create_client
 from openai_client.config import first_env_var
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import HttpUrl
 from skill_library import Skill
 from skill_library.routine_stack import RoutineStack
 from skill_library.run_context import LogEmitter, RunContext
@@ -29,51 +24,6 @@ async def test_poem_feedback():
     )
     language_model = create_client(service_config)
 
-    class Artifact(BaseModel):
-        student_poem: str = Field(description="The acrostic poem written by the student.")
-        initial_feedback: str = Field(description="Feedback on the student's final revised poem.")
-        final_feedback: str = Field(description="Feedback on how the student was able to improve their poem.")
-        inappropriate_behavior: list[str] = Field(
-            description=dedent("""
-                List any inappropriate behavior the student attempted while chatting with you.
-                It is ok to leave this field Unanswered if there was none.
-                """)
-        )
-
-    definition = GCDefinition(
-        artifact_schema=Artifact.model_json_schema(),
-        rules=[
-            "DO NOT write the poem for the student.",
-            "Terminate the conversation immediately if the students asks for harmful or inappropriate content.",
-        ],
-        conversation_flow=dedent("""
-            1. Start by explaining interactively what an acrostic poem is.
-            2. Then give the following instructions for how to go ahead and write one:
-                1. Choose a word or phrase that will be the subject of your acrostic poem.
-                2. Write the letters of your chosen word or phrase vertically down the page.
-                3. Think of a word or phrase that starts with each letter of your chosen word or phrase.
-                4. Write these words or phrases next to the corresponding letters to create your acrostic poem.
-            3. Then give the following example of a poem where the word or phrase is HAPPY:
-                Having fun with friends all day,
-                Awesome games that we all play.
-                Pizza parties on the weekend,
-                Puppies we bend down to tend,
-                Yelling yay when we win the game
-            4. Finally have the student write their own acrostic poem using the word or phrase of their choice. Encourage them
-            to be creative and have fun with it. After they write it, you should review it and give them feedback on what they
-            did well and what they could improve on. Have them revise their poem based on your feedback and then review it again.
-        """),
-        conversation_context=dedent("""
-            You are working 1 on 1 a 4th grade student who is chatting with you in the computer lab at school while being
-            supervised by their teacher.
-        """),
-        resource_constraint=ResourceConstraint(
-            quantity=10,
-            unit=ResourceConstraintUnit.TURNS,
-            mode=ResourceConstraintMode.EXACT,
-        ),
-    )
-
     # Configure the skill and its dependencies.
     drive = Drive(DriveConfig(root=Path(__file__).parent / ".data"))
     skill = GuidedConversationSkill(
@@ -81,14 +31,16 @@ async def test_poem_feedback():
     )
     skill_dependency_tree: dict[str, Skill] = {"guided_conversation": skill}
 
+    # Create a routine run function.
+    async def run_routine(name: str, vars: dict[str, Any] | None = None) -> Any:
+        await skill_registry.run_routine_by_designation(runContext, name, vars)
+
     # Set up a run context.
     path = Path(__file__).parent / ".data"
     drive = Drive(DriveConfig(root=path))
     routine_stack = RoutineStack(drive)
     skill_registry = SkillRegistry(skill_dependency_tree, routine_stack)
-    runContext = RunContext(
-        "test-session-1", drive, LogEmitter().emit, routine_stack, skill_registry.run_routine_by_designation
-    )
+    runContext = RunContext("test-session-1", drive, LogEmitter().emit, routine_stack, run_routine)
 
     # Run the skill with it's context.
     await skill.conversation_init_function(context=runContext)
