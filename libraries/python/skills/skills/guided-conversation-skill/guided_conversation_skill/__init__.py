f423b21a586d9819c8cff9074066b1419e47604b
Paul Payne <paul@payne.io>
2024-12-12 18:06:08 +0000

    Updates some tests.

--- libraries/python/skills/skills/guided-conversation-skill/guided_conversation_skill/__init__.py
+++ libraries/python/skills/skills/guided-conversation-skill/guided_conversation_skill/__init__.py
@@ -1,5 +1,11 @@
+from .definition import GCDefinition
 from .guided_conversation_skill import GuidedConversationSkill
+from .resources import ResourceConstraint, ResourceConstraintMode, ResourceConstraintUnit
 
 __all__ = [
     "GuidedConversationSkill",
+    "GCDefinition",
+    "ResourceConstraint",
+    "ResourceConstraintMode",
+    "ResourceConstraintUnit",
 ]
