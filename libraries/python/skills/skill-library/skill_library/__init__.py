9ff70b912c8a807fd22009cfe4afe2a98d644cb5
Paul Payne <paul@payne.io>
2025-01-08 17:40:48 +0000

    Adds run context providers and ensures run context is available in all action functions.

--- libraries/python/skills/skill-library/skill_library/__init__.py
+++ libraries/python/skills/skill-library/skill_library/__init__.py
@@ -1,16 +1,24 @@
 from context import Context
 
+from .actions import ActionCallable
 from .assistant import Assistant
+from .chat_driver_helpers import ChatDriverFunctions
 from .routine import InstructionRoutine, ProgramRoutine, RoutineTypes, StateMachineRoutine
-from .skill import EmitterType, Skill
+from .run_context import RunContext, RunContextProvider
+from .skill import EmitterType, Skill, SkillDefinition
 
 __all__ = [
+    "ActionCallable",
     "Assistant",
+    "ChatDriverFunctions",
     "Context",
     "EmitterType",
     "StateMachineRoutine",
     "InstructionRoutine",
     "ProgramRoutine",
     "RoutineTypes",
+    "RunContext",
+    "RunContextProvider",
     "Skill",
+    "SkillDefinition",
 ]
