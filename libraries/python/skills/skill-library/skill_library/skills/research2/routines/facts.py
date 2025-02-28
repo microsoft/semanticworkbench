13a9dc88ab99fe57d337cafee5da17b62b517f9b
Paul Payne <paul@payne.io>
2025-02-28 19:55:08 +0000

    Adds resesearch2 skill

---
+++ libraries/python/skills/skill-library/skill_library/skills/research2/routines/facts.py
@@ -0,0 +1,153 @@
+from typing import Any, cast
+
+from openai_client import (
+    CompletionError,
+    create_assistant_message,
+    create_system_message,
+    create_user_message,
+    extra_data,
+    format_with_liquid,
+    make_completion_args_serializable,
+    message_content_from_completion,
+    validate_completion,
+)
+from skill_library import AskUserFn, EmitFn, RunContext, RunRoutineFn
+from skill_library.logging import logger
+from skill_library.skills.research2.research_skill import ResearchSkill
+
+INITIAL_SYSTEM_PROMPT = """
+Below I will present you a topic.
+
+You will now build a comprehensive preparatory survey of which facts we have at our disposal and which ones we still need.
+To do so, you will have to read the topic and identify things that must be discovered in order to successfully complete it.
+Don't make any assumptions. For each item, provide a thorough reasoning. Here is how you will structure this survey:
+
+---
+### 1. Facts given in the topic
+List here the specific facts given in the topic that could help you (there might be nothing here).
+
+### 2. Facts to look up
+List here any facts that we may need to look up.
+Also list where to find each of these, for instance a website, a file... - maybe the topic contains some sources that you should re-use here.
+
+### 3. Facts to derive
+List here anything that we want to derive from the above by logical reasoning, for instance computation or simulation.
+
+Keep in mind that \"facts\" will typically be specific names, dates, values, etc. Your answer should use the below headings:
+### 1. Facts given in the task
+### 2. Facts to look up
+### 3. Facts to derive
+
+Do not add anything else.
+
+Here is the topic that all facts should relate to:
+
+```
+{TOPIC}
+```
+
+Now begin!
+"""
+
+UPDATE_SYSTEM_PROMPT = """
+You will now build a comprehensive preparatory survey of which facts we have at our disposal and which ones we still need.
+
+Earlier we've built a list of facts.
+
+But since in your previous steps you may have learned useful new facts or invalidated some false ones.
+
+Please update your list of facts based on the previous history, and provide these headings:
+
+### 1. Facts given in the topic
+### 2. Facts that we have learned
+### 3. Facts still to look up
+### 4. Facts still to derive
+
+Do not add anything else.
+
+Reminder, here is the topic that all facts should relate to:
+
+```
+{TOPIC}
+```
+
+Now begin!
+"""
+
+
+async def main(
+    context: RunContext,
+    routine_state: dict[str, Any],
+    emit: EmitFn,
+    run: RunRoutineFn,
+    ask_user: AskUserFn,
+    topic: str,
+    plan: str = "",
+    facts: str = "",
+    observations: list[str] = [],
+) -> str:
+    """Gather facts related to an ongoing research project based on observations we gather in the process of researching."""
+
+    research_skill = cast(ResearchSkill, context.skills["common"])
+    language_model = research_skill.config.language_model
+
+    system_prompt = UPDATE_SYSTEM_PROMPT if facts else INITIAL_SYSTEM_PROMPT
+
+    completion_args = {
+        "model": "gpt-4o",
+        "messages": [
+            create_system_message(
+                format_with_liquid(system_prompt, vars={"TOPIC": topic}),
+            ),
+            create_user_message(
+                f"Topic: {topic}",
+            ),
+        ],
+    }
+
+    if plan:
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Plan: {plan}",
+            )
+        )
+
+    if facts:
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Facts: \n```{facts}\n```\n",
+            )
+        )
+
+    if observations:
+        all_observations = "\n- ".join(observations)
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Observations: \n```{all_observations}\n```\n",
+            )
+        )
+
+    logger.debug("Completion call.", extra=extra_data(make_completion_args_serializable(completion_args)))
+    metadata = {}
+    metadata["completion_args"] = make_completion_args_serializable(completion_args)
+    try:
+        completion = await language_model.beta.chat.completions.parse(
+            **completion_args,
+        )
+        validate_completion(completion)
+        logger.debug("Completion response.", extra=extra_data({"completion": completion.model_dump()}))
+        metadata["completion"] = completion.model_dump()
+    except Exception as e:
+        completion_error = CompletionError(e)
+        metadata["completion_error"] = completion_error.message
+        logger.error(
+            completion_error.message,
+            extra=extra_data({"completion_error": completion_error.body, "metadata": context.metadata_log}),
+        )
+        raise completion_error from e
+    else:
+        content = message_content_from_completion(completion).strip().strip('"')
+        metadata["content"] = content
+        return content
+    finally:
+        context.log("facts", metadata)
---
+++ libraries/python/skills/skill-library/skill_library/skills/research2/routines/search_plan.py
@@ -0,0 +1,125 @@
+from typing import Any, cast
+
+from openai_client import (
+    CompletionError,
+    create_assistant_message,
+    create_system_message,
+    create_user_message,
+    extra_data,
+    format_with_liquid,
+    make_completion_args_serializable,
+    message_content_from_completion,
+    validate_completion,
+)
+from skill_library import AskUserFn, EmitFn, RunContext, RunRoutineFn
+from skill_library.logging import logger
+from skill_library.skills.research2.research_skill import ResearchSkill
+
+INITIAL_SYSTEM_PROMPT = """
+You are a world expert at making efficient plans to solve any task using a set of carefully crafted tools.
+
+Now for the given task, develop a step-by-step high-level plan taking into account the above inputs and list of facts.
+This plan should involve individual tasks based on the available tools, that if executed correctly will yield the correct answer.
+Do not skip steps, do not add any superfluous steps. Only write the high-level plan.
+
+Here is your topic:
+
+{TOPIC}
+
+List of facts that you know:
+
+{FACTS}
+
+Now begin! Write your plan below.
+"""
+
+UPDATE_SYSTEM_PROMPT = """
+You're still working towards completing this research:
+
+{TOPIC}
+
+Now for the given topic, develop a step-by-step high-level plan taking into account the above inputs and list of facts.
+This plan should involve individual tasks that if executed correctly will yield the correct answer.
+
+If you decide that the research topic has been completed, respond only with <DONE>.
+"""
+
+
+async def main(
+    context: RunContext,
+    routine_state: dict[str, Any],
+    emit: EmitFn,
+    run: RunRoutineFn,
+    ask_user: AskUserFn,
+    topic: str,
+    plan: str = "",
+    facts: str = "",
+    observations: list[str] = [],
+) -> tuple[str, bool]:
+    """Make a search plan for a research project."""
+
+    research_skill = cast(ResearchSkill, context.skills["common"])
+    language_model = research_skill.config.language_model
+
+    system_prompt = UPDATE_SYSTEM_PROMPT if plan else INITIAL_SYSTEM_PROMPT
+
+    completion_args = {
+        "model": "gpt-4o",
+        "messages": [
+            create_system_message(
+                format_with_liquid(system_prompt, vars={"TOPIC": topic, "FACTS": facts}),
+            ),
+            create_user_message(
+                f"Topic: {topic}",
+            ),
+        ],
+    }
+
+    if plan:
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Plan: {plan}",
+            )
+        )
+
+    if facts:
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Here is the up-to-date list of facts that you know:: \n```{facts}\n```\n",
+            )
+        )
+
+    if observations:
+        all_observations = "\n- ".join(observations)
+        completion_args["messages"].append(
+            create_assistant_message(
+                f"Observations: \n```{all_observations}\n```\n",
+            )
+        )
+
+    logger.debug("Completion call.", extra=extra_data(make_completion_args_serializable(completion_args)))
+    metadata = {}
+    metadata["completion_args"] = make_completion_args_serializable(completion_args)
+    try:
+        completion = await language_model.beta.chat.completions.parse(
+            **completion_args,
+        )
+        validate_completion(completion)
+        logger.debug("Completion response.", extra=extra_data({"completion": completion.model_dump()}))
+        metadata["completion"] = completion.model_dump()
+    except Exception as e:
+        completion_error = CompletionError(e)
+        metadata["completion_error"] = completion_error.message
+        logger.error(
+            completion_error.message,
+            extra=extra_data({"completion_error": completion_error.body, "metadata": context.metadata_log}),
+        )
+        raise completion_error from e
+    else:
+        content = message_content_from_completion(completion).strip().strip('"')
+        metadata["content"] = content
+        if content == "<DONE>":
+            return content, True
+        return content, False
+    finally:
+        context.log("plan", metadata)
