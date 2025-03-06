591ad5380f5ad12a88425eb55bc08fe017e58924
Paul Payne <paul@payne.io>
2025-03-06 00:41:09 +0000

    Adds web-research MCP to Code Assistant.

--- mcp-servers/mcp-server-web-research/mcp_server_web_research/config.py
+++ mcp-servers/mcp-server-web-research/mcp_server_web_research/config.py
@@ -1,14 +1,12 @@
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-from openai import AsyncAzureOpenAI, AsyncOpenAI
-from openai.lib.azure import AsyncAzureADTokenProvider
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     log_level: str = "INFO"
     data_folder: str = ".data"
-    azure_openai_endpoint: str = ""
+    azure_openai_endpoint: str = "gpt-4o"
     azure_openai_deployment: str = ""
+    azure_openai_reasoning_deployment: str = "o1-mini"
     bing_subscription_key: str = ""
     bing_search_url: str = "https://api.bing.microsoft.com/v7.0/search"
     serpapi_api_key: str = ""
@@ -27,29 +25,3 @@ required_settings = ["azure_openai_deployment", "azure_openai_endpoint", "bing_s
 for setting in required_settings:
     if not getattr(settings, setting):
         raise ValueError(f"Missing required setting: {setting}")
-
-
-# Azure OpenAI client
-
-
-def create_client(api_version: str = "2024-12-01-preview") -> AsyncOpenAI:
-    return AsyncAzureOpenAI(
-        azure_ad_token_provider=_get_azure_bearer_token_provider(),
-        azure_deployment=settings.azure_openai_deployment,
-        azure_endpoint=settings.azure_openai_endpoint,
-        api_version=api_version,
-    )
-
-
-_lazy_initialized_azure_bearer_token_provider = None
-
-
-def _get_azure_bearer_token_provider() -> AsyncAzureADTokenProvider:
-    global _lazy_initialized_azure_bearer_token_provider
-
-    if _lazy_initialized_azure_bearer_token_provider is None:
-        _lazy_initialized_azure_bearer_token_provider = get_bearer_token_provider(
-            DefaultAzureCredential(),
-            "https://cognitiveservices.azure.com/.default",
-        )
-    return _lazy_initialized_azure_bearer_token_provider
