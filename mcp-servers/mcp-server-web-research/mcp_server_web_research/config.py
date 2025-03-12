33c6f4e5fc691c7ccd9c5b40bc0d53903d3091e9
Mark Waddle <mark@markwaddle.com>
2025-03-11 17:09:02 -0700

    Fix warning

--- mcp-servers/mcp-server-web-research/mcp_server_web_research/config.py
+++ mcp-servers/mcp-server-web-research/mcp_server_web_research/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -11,10 +11,6 @@ class Settings(BaseSettings):
     bing_search_url: str = "https://api.bing.microsoft.com/v7.0/search"
     serpapi_api_key: str = ""
     huggingface_token: str = ""
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-    )
 
 
 settings = Settings()
