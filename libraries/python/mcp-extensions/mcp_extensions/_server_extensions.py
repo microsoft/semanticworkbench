6ac1fd3a384b34e7cb1254afa4048da0d4c85b8f
Mark Waddle <mark@markwaddle.com>
2025-03-26 18:22:05 -0700

    Adds write_client_resource function

--- libraries/python/mcp-extensions/mcp_extensions/_server_extensions.py
+++ libraries/python/mcp-extensions/mcp_extensions/_server_extensions.py
@@ -1,3 +1,5 @@
+import base64
+
 from mcp import (
     ErrorData,
     ListResourcesRequest,
@@ -7,8 +9,10 @@ from mcp import (
     ServerSession,
 )
 from mcp.server.fastmcp import Context
-from mcp.types import ReadResourceRequestParams
+from mcp.types import BlobResourceContents, ReadResourceRequestParams, TextResourceContents
 from pydantic import AnyUrl
+
+from mcp_extensions import WriteResourceRequest, WriteResourceRequestParams, WriteResourceResult
 
 
 async def list_client_resources(fastmcp_server_context: Context) -> ListResourcesResult | ErrorData:
@@ -28,8 +32,8 @@ async def list_client_resources(fastmcp_server_context: Context) -> ListResource
 
 async def read_client_resource(fastmcp_server_context: Context, uri: AnyUrl) -> ReadResourceResult | ErrorData:
     """
-    Lists all resources that the client has. This is reliant on the client supporting
-    the experimental `resources/list` method.
+    Reads a resource from the client. This is reliant on the client supporting
+    the experimental `resources/read` method.
     """
     server_session: ServerSession = fastmcp_server_context.session
 
@@ -41,4 +45,29 @@ async def read_client_resource(fastmcp_server_context: Context, uri: AnyUrl) -> 
             ),
         ),  # type: ignore - this is an experimental method not explicitly defined in the mcp package
         result_type=ReadResourceResult,
+    )
+
+
+async def write_client_resource(
+    fastmcp_server_context: Context, uri: AnyUrl, content_type: str, content: bytes
+) -> WriteResourceResult | ErrorData:
+    """
+    Writes a client resource. This is reliant on the client supporting the experimental `resources/write` method.
+    """
+    server_session: ServerSession = fastmcp_server_context.session
+
+    if content_type.startswith("text/"):
+        content = TextResourceContents(uri=uri, mimeType=content_type, text=content.decode("utf-8"))
+    else:
+        content = BlobResourceContents(uri=uri, mimeType=content_type, blob=base64.b64encode(content).decode("utf-8"))
+
+    return await server_session.send_request(
+        request=WriteResourceRequest(
+            method="resources/write",
+            params=WriteResourceRequestParams(
+                uri=uri,
+                contents=content,
+            ),
+        ),  # type: ignore - this is an experimental method not explicitly defined in the mcp package
+        result_type=WriteResourceResult,
     )
