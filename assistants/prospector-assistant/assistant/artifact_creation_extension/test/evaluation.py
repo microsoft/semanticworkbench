594d8504ece1e66ca0e461b3a0e8e83c6b262a10
Mark Waddle <markwaddle@users.noreply.github.com>
2025-04-18 13:54:22 -0700

    Remove dependency on torch (#492)
    The version that is installable on Mac x86 is no longer considered
    secure, and we don't need these tests that depend on it anyways, except
    for posterity.

--- assistants/prospector-assistant/assistant/artifact_creation_extension/test/evaluation.py
+++ assistants/prospector-assistant/assistant/artifact_creation_extension/test/evaluation.py
@@ -1,17 +1,18 @@
-from sentence_transformers import SimilarityFunction
-
-
 def sentence_cosine_similarity(sentence1: str, sentence2: str) -> float:
     """Calculate the cosine similarity between two sentences."""
 
-    from sentence_transformers import SentenceTransformer
+    raise RuntimeError(
+        "This function is disabled because upgrading torch doesn't work on Mac X86, and the torch that does work is no longer secure."
+    )
 
-    model = SentenceTransformer("all-mpnet-base-v2", similarity_fn_name=SimilarityFunction.COSINE)
+    # from sentence_transformers import SentenceTransformer, SimilarityFunction
 
-    sentence1_embeddings = model.encode([sentence1])
-    sentence2_embeddings = model.encode([sentence2])
+    # model = SentenceTransformer("all-mpnet-base-v2", similarity_fn_name=SimilarityFunction.COSINE)
 
-    similarities = model.similarity(sentence1_embeddings[0], sentence2_embeddings[0])
-    similarity = similarities[0][0]
+    # sentence1_embeddings = model.encode([sentence1])
+    # sentence2_embeddings = model.encode([sentence2])
 
-    return similarity.item()
+    # similarities = model.similarity(sentence1_embeddings[0], sentence2_embeddings[0])
+    # similarity = similarities[0][0]
+
+    # return similarity.item()
